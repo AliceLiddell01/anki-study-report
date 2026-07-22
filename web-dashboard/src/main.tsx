f73255a54b5ba7094984718e1ef5f8afd5bea61e
AliceLiddell01 <leaf.fairy@proton.me>
2026-07-23 03:19:10 +0700

    Remove the noncanonical preview highlighter implementation

--- web-dashboard/src/main.tsx
+++ web-dashboard/src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
-import { installCardCodeHighlighting } from "./lib/cardCodeHighlighting";
 import "./styles.css";
 import "./styles/cardsInbox.css";
 import "./styles/prototypeV323.css";
-
-installCardCodeHighlighting();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
