ea431acee24901c69460f6a6d1dc0393912eeab3
AliceLiddell01 <leaf.fairy@proton.me>
2026-07-26 02:25:10 +0700

    Remove the rejected global prototype overlay

--- web-dashboard/src/main.tsx
+++ web-dashboard/src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import "./i18n";
 import "./styles.css";
 import "./styles/cardsInbox.css";
-import "./styles/prototypeV323.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
