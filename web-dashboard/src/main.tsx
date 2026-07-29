d4fe8b5addabdc7db6d88d117cc4f864646fd19e
AliceLiddell01 <leaf.fairy@proton.me>
2026-07-29 22:34:49 +0700

    Fix Cards locale typography loading

--- web-dashboard/src/main.tsx
+++ web-dashboard/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./i18n";
 import "./styles.css";
 import "./styles/cardsInbox.css";
+import "./styles/cardsTypography.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
